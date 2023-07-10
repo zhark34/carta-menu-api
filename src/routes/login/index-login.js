338e5a8467ab8b493e18eb414583fa89e57d760e
zhark34 <zhark34>
2023-07-10 01:18:21 -0300

    update para loguear con react

--- src/routes/login/index-login.js
+++ src/routes/login/index-login.js
@@ -20,15 +20,29 @@ app.use(session({
 }));
 
 app.post('/loguear', auth, (req, res)=>{
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header("Access-Control-Allow-Headers","Origin, X-Requeted-With, Content-Type, Accept, Authorization, RBR");
   admins.find({ usuario: req.body.user, password: req.body.pass })
   .exec()
-  .then(user => {
-    if (user.length === 0) {
-      res.send("error al loguear");
-    } else{
-      req.session.user = req.body.user;
-      req.session.admin = req.body.password;
-      res.send("logueado");
+  .then(user => {    
+    if (req.body.user && req.body.password) {
+        var query = {$and: [{user: req.body.user}, {password: req.body.password}]};        
+        esquema.find(query, (err, data) =>{
+            if (err) return res.status(200).send({"login":0,"error":"Error Interno"});            
+            if(data.length>0){       
+                req.session.user = req.body.user;
+                req.session.admin = req.body.password;
+                const f = Date.now();
+                res.status(200).send({"login":1,"name":data[0].name,"token":"token-secreto-"+f});
+            }
+            else{
+                res.status(200).send({"login":0,"Error":"Usuario o Contraseña incorrecto/s. Intenta con otros datos."});
+            }            
+        });
+    }
+    else{
+        res.status(200).send({"login":0,"error":"User y/o password no son correctos"});
     }
   })
 });
