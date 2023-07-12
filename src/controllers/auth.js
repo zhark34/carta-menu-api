7845dddb0dbeeaf7047b6836733af8c71b5f88af
zhark34 <zhark34>
2023-07-12 17:24:47 -0300

    cambio en status en auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,7 +4,7 @@ const auth = function(req, res, next) {
       return next();
     }
     else{
-      return res.status(401).send("<h1>No has sido autorizado, amigo. O tu sesion expiró.</h1><p><a href='/'>Regresar al login</a></p>");
+      return res.send("<h1>No has sido autorizado, amigo. O tu sesion expiró.</h1><p><a href='/'>Regresar al login</a></p>");
     }
 };
 
