6baa4de2ba667fc649cd550d0cb3d0a1a0a9ee35
zhark34 <zhark34>
2023-07-13 23:36:04 -0300

    agregado ruta para validar tokens

--- src/routes/login/index-login.js
+++ src/routes/login/index-login.js
@@ -19,6 +19,8 @@ app.use(session({
     cookie: { maxAge: 60 * 60 * 1000 }
 }));
 
+const secretKey = 'Token_Login';
+
 app.post('/loguear', (req, res)=>{
 
   res.header('Access-Control-Allow-Origin', '*');
@@ -37,7 +39,7 @@ app.post('/loguear', (req, res)=>{
       req.session.user = req.body.user;
       req.session.pass = req.body.pass;
       const f = Date.now();
-      res.status(200).send({login:1,message:"Aceso concedido",token:"token-secreto-"+f});
+      res.status(200).send({login:1,message:"Aceso concedido", token:"token-secreto-"+f});
     }
     else{
         res.status(200).send({login:0,message:"User y/o password no son correctos"});
@@ -45,11 +47,25 @@ app.post('/loguear', (req, res)=>{
   })
 });
 
-app.all('/chek', auth, (req, res)=>{
+app.post('/chek', (req, res)=>{
+  
   res.header('Access-Control-Allow-Origin', '*');
-  const f = Date.now();
-  const data = [{login:1,message:"Aceso concedido",token:"token-secreto-"+f}];
-  res.status(200).send(data);
+  res.header('Access-Control-Allow-Methods', 'POST');
+  res.header("Access-Control-Allow-Headers","Origin, X-Requeted-With, Content-Type, Accept, Authorization, RBR");
+  
+  const token = req.body.token;
+
+  try {
+      const decoded = jwt.verify(token, secretKey);
+      
+      const userId = decoded.userId;
+      
+      res.status(200).json({ login: 1, message: "Acceso concedido", userId });
+  } catch (err) {
+      
+      res.status(401).json({ login: 0, message: "Acceso no autorizado" });
+  }
+
 });
 
 app.all('/logout', function (req, res) {
