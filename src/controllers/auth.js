2a3c2cbaec60aca5a332bdc3539dc89f86ece1be
zhark34 <zhark34>
2023-07-14 16:24:21 -0300

    cambio en el token

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,7 +5,7 @@ const auth = function(req, res, next) {
       return res.status(401).json({ message: 'Acceso no autorizado. Token no proporcionado.' });
     }
     try {
-      const decoded = jwt.verify(token, 'mi_clave_secreta');
+      const decoded = jwt.verify(token, 'token-secreto-');
       req.userId = decoded.userId;
       req.username = decoded.username;
       res.status(200).send({ message: "Acceso concedido", token });
